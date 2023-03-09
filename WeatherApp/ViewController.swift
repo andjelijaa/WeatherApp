dc00a2f87201f996048adeb67122030f7b6ae56e
Andjelija <andjelija.djordjevic1997@gmail.com>
2023-03-09 08:29:07 +0100

    Adding initial project

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Andjelija on 8.3.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
